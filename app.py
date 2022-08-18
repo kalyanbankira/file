ec7baca6c3cfcf7b733107a02accee00ab31c4b8
kalyanbankira <111225686+kalyanbankira@users.noreply.github.com>
2022-08-18 15:44:04 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,76 @@
+# S2.1: Open Sublime text editor, create a new Python file, copy the following code in it and save it as 'improved_iris_app.py'.
+# Importing the necessary libraries.
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import streamlit as st
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
+from sklearn.linear_model import LogisticRegression  
+from sklearn.ensemble import RandomForestClassifier
+
+# Loading the dataset.
+iris_df = pd.read_csv("iris-species.csv")
+
+# Adding a column in the Iris DataFrame to resemble the non-numeric 'Species' column as numeric using the 'map()' function.
+# Creating the numeric target column 'Label' to 'iris_df' using the 'map()' function.
+iris_df['Label'] = iris_df['Species'].map({'Iris-setosa': 0, 'Iris-virginica': 1, 'Iris-versicolor':2})
+
+# Creating features and target DataFrames.
+X = iris_df[['SepalLengthCm','SepalWidthCm', 'PetalLengthCm', 'PetalWidthCm']]
+y = iris_df['Label']
+
+# Splitting the dataset into train and test sets.
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.33, random_state = 42)
+
+# Creating an SVC model. 
+svc_model = SVC(kernel = 'linear')
+svc_model.fit(X_train, y_train)
+
+# Creating a Logistic Regression model. 
+rf_clf = RandomForestClassifier(n_jobs = -1, n_estimators = 100)
+rf_clf.fit(X_train, y_train)
+
+# Creating a Random Forest Classifier model.
+log_reg = LogisticRegression(n_jobs = -1)
+log_reg.fit(X_train, y_train)
+
+@st.cache()
+def prediction(model, sepal_length, sepal_width, petal_length, petal_width):
+  species = model.predict([[sepal_length, sepal_width, petal_length, petal_width]])
+  species = species[0]
+  if species == 0:
+    return "Iris-setosa"
+  elif species == 1:
+    return "Iris-virginica"
+  else:
+    return "Iris-versicolor" 
+
+st.sidebar.title("Iris Flower Prediction App")
+
+s_len = st.sidebar.slider("sepal_length",float(iris_df['SepalLengthCm'].min()),float(iris_df['SepalLengthCm'].max()))
+s_wid = st.sidebar.slider("sepal_width",float(iris_df['SepalWidthCm'].min()),float(iris_df['SepalWidthCm'].max()))
+p_len = st.sidebar.slider("petal_length",float(iris_df['PetalLengthCm'].min()),float(iris_df['PetalLengthCm'].max()))
+p_wid = st.sidebar.slider("petal_width",float(iris_df['PetalWidthCm'].min()),float(iris_df['PetalWidthCm'].max()))
+
+st.sidebar.subheader("Choose_classifier")
+
+classifier = st.sidebar.selectbox('Classifier',('Support Vector Machine','Logistic Regression','Random Forest Classifier'))
+
+if st.sidebar.button('Predict'):
+  if classifier == 'Support Vector Machine':
+    species_ab = prediction(svc_model,s_len,s_wid,p_len,p_wid)
+    score1 = svc_model.score(X_train,y_train)
+  
+  elif classifier == 'Logistic Regression':
+    species_ab = prediction(log_reg,s_len,s_wid,p_len,p_wid)
+    score1 = log_reg.score(X_train,y_train)
+  
+  else:
+    species_ab = prediction(rf_clf,s_len,s_wid,p_wid,p_len)
+    score1 = rf_clf.score(X_train,y_train)
+      
+
+  st.write("species_predicted of this model  is :",species_ab)
+  st.write("Score of the is :",score1)     
